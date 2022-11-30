24a73c46a37eaa1b24544a7b9ca222e3702cde94
te06008 <te06008@naver.com>
2022-11-30 10:15:55 +0900

    remove
    안쓰는 파일 제거

--- src/components/main/about/AboutMe.tsx
+++ src/components/main/about/AboutMe.tsx
@@ -2,13 +2,17 @@ import Styled from './AboutMe.styled';
 import IntroduceMe from './IntroduceMe';
 
 import myImg from '../../../assets/me.png';
+import { Helmet } from 'react-helmet-async';
 
 function AboutMe() {
   return (
     <Styled.AboutMe>
+      <Helmet>
+        <title>Te06008 | About</title>
+      </Helmet>
       <Styled.IntroduceWrapper>
         <IntroduceMe />
-        <img src={myImg} alt='이미지를 불러올수 없습니다...' />
+        <img src={myImg} alt="이미지를 불러올수 없습니다..." />
       </Styled.IntroduceWrapper>
     </Styled.AboutMe>
   );

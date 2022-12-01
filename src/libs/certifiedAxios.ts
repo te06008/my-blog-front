9017a36ab73b8ba2bf6325c8943bb6ad4c8e4981
te06008 <te06008@naver.com>
2022-12-01 21:52:22 +0900

    asdf

--- src/libs/certifiedAxios.ts
+++ src/libs/certifiedAxios.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 const certifiedAxios = axios.create();
 certifiedAxios.defaults.withCredentials = true;
-
+certifiedAxios.defaults.headers['Access-Control-Allow-Headers'] =
+  'http://te06008.me';
 export default certifiedAxios;
